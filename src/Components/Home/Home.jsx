052f85c107ffea1ccc1913db5d64e5df757dfd3b
fabien <fabienboy661@gmail.com>
2023-01-29 13:09:33 +0300

    udpate

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ import {AiOutlineInstagram} from 'react-icons/ai'
 import {SiGmail} from 'react-icons/si'
 import {AiFillGooglePlusCircle} from 'react-icons/ai'
 import {AiOutlineAmazon} from 'react-icons/ai'
-import { Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
@@ -18,9 +18,8 @@ const Home = () => {
 
   return (
     <section id='menu'>
-
-        <h2 data-aos="fade-right">Madagascar Tiako</h2>
-        <h4 data-aos="fade-left">ELIA BEBEKO TIAKO</h4>
+        <h2 data-aos="fade-right">Bienvenue</h2>
+        <h4 data-aos="fade-left">NOT UPTADE</h4>
         <p data-aos="fade-up">Nous vous offrons des services fiable et rapide avec des equipes specialisees </p>
         <p data-aos="fade-up">et des materieles de luxe</p>
         <Link to="/forms" data-aos="fade-up" className='btn-reservation'>Reserver Maintenant</Link>
