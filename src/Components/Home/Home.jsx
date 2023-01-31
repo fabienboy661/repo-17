1b8bd25ff15693f62bdf905c488cff5ea74b479f
fabien <fabienboy661@gmail.com>
2023-01-31 17:16:22 +0300

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
   return (
     <section id='menu'>
-        <h2 data-aos="fade-right">Bienvenue</h2>
+        <h2 data-aos="fade-right">Bienvenue ENI</h2>
         <h4 data-aos="fade-left">Site de Voyage a Madagascar</h4>
         <p data-aos="fade-up">Nous vous offrons des services fiable et rapide avec des equipes specialisees </p>
         <p data-aos="fade-up">et des materieles de luxe</p>
